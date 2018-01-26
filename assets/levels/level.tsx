d53b8911171ef1ffd3ad71f1601ca8eaa70d3583
Kjili <Kjili@users.noreply.github.com>
2018-01-27 00:26:51 +0100

    Fix path

--- assets/levels/level.tsx
+++ assets/levels/level.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="level" tilewidth="64" tileheight="64" tilecount="100" columns="10">
- <image source="../../assets/sprites/level.png" width="640" height="640"/>
+ <image source="../sprites/level.png" width="640" height="640"/>
  <terraintypes>
   <terrain name="Sand" tile="0"/>
  </terraintypes>
