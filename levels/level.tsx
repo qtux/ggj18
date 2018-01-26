fe186a41b9dce8b8f8ad32965bcad62557d8a79e
Sebastian Artz <sebastian.artz85@gmail.com>
2018-01-26 22:45:53 +0100

    Add first dummy level Tiled file + json export

---
+++ levels/level.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="level" tilewidth="64" tileheight="64" tilecount="100" columns="10">
+ <image source="../img/level.png" width="640" height="640"/>
+</tileset>
