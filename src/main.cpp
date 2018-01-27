63e95d2ce20c1c9d364622a5533721b2b37828e3
Kjili <Kjili@users.noreply.github.com>
2018-01-27 09:17:18 +0100

    Add *** animation to intro

--- src/main.cpp
+++ src/main.cpp
@@ -48,11 +48,8 @@ int main() {
 	fixtureDef.shape = &shape;
 	body->CreateFixture(&fixtureDef);
 	
-	// show intro
-	int i = 0;
-	
 	// the game view (full window)
-//myView.setViewport(sf::FloatRect(0, 0, , 768./1280.));
+	//myView.setViewport(sf::FloatRect(0, 0, , 768./1280.));
 	//myView.zoom(1280./768.);
 	myView.setSize(1707,1280);//1707 = aspect ratio * 1280
 	
@@ -60,6 +57,9 @@ int main() {
 	// clock to determine fixed logic rate
 	sf::Clock clock;
 	float timeStep = 1.0f / 60.0f;
+	int i = 0;
+	bool switched = true;
+	sf::Texture img;
 	//start game
 	GameStates gameState = GameStates::GAME_STATE_INTRO;
 	while (window.isOpen()) {
@@ -67,20 +67,45 @@ int main() {
 		while (window.pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window.close();
-			if (event.type == sf::Event::EventType::KeyPressed || event.type == sf::Event::EventType::MouseButtonPressed)
+			if (event.type == sf::Event::EventType::KeyPressed || event.type == sf::Event::EventType::MouseButtonPressed) {
+				switched = true;
 				++i;
+			}
 		}
 		window.clear(sf::Color::Black);
 		window.setView(myView);
 		
 		// fixed rate for logic functions
+		float initialZoomIntro = 0.5;
 		if (clock.getElapsedTime() > sf::seconds(timeStep)) {
 			// reset clock if more than a 1/60 seconds have passed
 			sf::Time deltaT = clock.restart();
 			switch(gameState) {
-				case GameStates::GAME_STATE_INTRO:
+				case GameStates::GAME_STATE_INTRO: {
 					// functions
+					if (switched && (i >= 2 || img.loadFromFile("assets/intro/intro_" + std::to_string(i) + ".png") != true)) {
+						gameState = GameStates::GAME_STATE_LEVEL;
+						break;
+					}
+					if (switched) {
+						switched = false;
+						myView.setSize(img.getSize().x, img.getSize().y);
+						if (i == 0) {
+							//myView.setCenter(img.getSize().x/2, img.getSize().y*3*(initialZoomIntro/2));
+							myView.setCenter(img.getSize().x/2, img.getSize().y/2);
+							myView.zoom(initialZoomIntro);
+						} else {
+							myView.setCenter(img.getSize().x/2, img.getSize().y/2);
+						}
+					}
+					int delay = 300;
+					if (i == 0 && myView.getSize().y <= img.getSize().y) {
+						myView.zoom(1 + initialZoomIntro/delay);
+					} else {
+						myView.setCenter(img.getSize().x/2, img.getSize().y/2);
+					}
 					break;
+				}
 				case GameStates::GAME_STATE_LEVEL:
 					// functions
 					world.Step(deltaT.asSeconds(), 8, 3);
@@ -92,14 +117,7 @@ int main() {
 		// draw as often as possible
 		switch(gameState) {
 			case GameStates::GAME_STATE_INTRO: {
-				sf::Texture img;
-				if(i >= 2 || img.loadFromFile("assets/intro/intro_" + std::to_string(i) + ".png") != true) {
-					gameState = GameStates::GAME_STATE_LEVEL;
-					break;
-				}
 				sf::Sprite sprite(img);
-				myView.setSize(img.getSize().x, img.getSize().y);
-				myView.setCenter(img.getSize().x/2, img.getSize().y/2);
 				window.setView(myView);
 				window.draw(sprite);
 				break;
