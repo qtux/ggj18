66dbaa2e87e11a252a8898795142ae3032b36b77
Johannes Mattmann <johannes.mattmann@googlemail.com>
2018-01-28 15:38:26 +0100

    minor fixes

--- assets/scripts/config.lua
+++ assets/scripts/config.lua
@@ -14,6 +14,7 @@ view_height = 20 * 64
 level_speed = 1.5
 level_file = "assets/levels/level0.tmx"
 start_level = 0
+debug_draw = 0
 
 -- player config
 jump_impulse = -0.5
@@ -21,4 +22,4 @@ player_friction = 0
 player_density = 1
 flying_gravity_scale = 0.2
 
-immortal = 3
+immortal = 0
